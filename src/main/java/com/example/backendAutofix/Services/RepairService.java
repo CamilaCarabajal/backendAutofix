fd78dec135d8150872e4c20957916b6fd30540db
CamilaCarabajal <camila.carabajal@usach.cl>
2024-04-08 15:45:17 -0400

    Funcion costo Reparaciones
    Se creo la funcion costoReparacion la cual retorna los valores de cada reparacion dependiendo del tipo de motor que tiene el vehiculo

--- src/main/java/com/example/backendAutofix/Services/RepairService.java
+++ src/main/java/com/example/backendAutofix/Services/RepairService.java
@@ -19,9 +19,134 @@ public class RepairService {
     @Autowired
     VehicleRepository vehicleRepository;
 
-    public int
-    public int recargoKilometraje (String reparacion){
-        VehicleEntity vehiculo = vehicleRepository.findByPatenteQuery(reparacion);
+    public RepairEntity createRepair(RepairEntity repair){
+        return repairRepository.save(repair);
+    }
+
+    public int costoReparaciones (RepairEntity reparacion, VehicleEntity vehiculo){
+        if(reparacion.getTipoReparacion() == 1){
+            if(vehiculo.getMotor() == "Gasolina" || vehiculo.getMotor() == "Diesel"){
+                return 120000;
+
+            }
+            else if(vehiculo.getMotor() == "Hibrido"){
+                return 180000;
+            }
+            else if(vehiculo.getMotor() == "Electrico"){
+                return 220000;
+            }
+
+        }
+        else if(reparacion.getTipoReparacion() == 2){
+            if(vehiculo.getMotor() == "Gasolina" || vehiculo.getMotor() == "Diesel"){
+                return 130000;
+            }
+            else if(vehiculo.getMotor() == "Hibrido"){
+                return 190000;
+            }
+            else if(vehiculo.getMotor() == "Electrico"){
+                return 230000;
+            }
+        }
+        else if(reparacion.getTipoReparacion() == 3){
+            if(vehiculo.getMotor() == "Gasolina"){
+                return 350000;
+            }
+            else if(vehiculo.getMotor() == "Diesel"){
+                return 450000;
+            }
+            else if(vehiculo.getMotor() == "Hibrido"){
+                return 700000;
+            }
+            else if (vehiculo.getMotor() == "Electrico"){
+                return 800000;
+            }
+        }
+        else if(reparacion.getTipoReparacion() == 4){
+            if(vehiculo.getMotor() == "Gasolina" || vehiculo.getMotor() == "Diesel"){
+                return 210000;
+            }
+            else if(vehiculo.getMotor() == "Hibrido" || vehiculo.getMotor() == "Electrico"){
+                return 300000;
+            }
+
+        }
+        else if(reparacion.getTipoReparacion() == 5){
+            if(vehiculo.getMotor() == "Gasolina" || vehiculo.getMotor() == "Diesel"){
+                return 150000;
+            }
+            else if(vehiculo.getMotor() == "Hibrido"){
+                return 200000;
+            }
+            else if(vehiculo.getMotor() == "Electrico"){
+                return 250000;
+            }
+        }
+        else if(reparacion.getTipoReparacion() == 6){
+            if(vehiculo.getMotor() == "Gasolina"){
+                return 100000;
+            }
+            else if(vehiculo.getMotor() == "Diesel"){
+                return 120000;
+            }
+            else if(vehiculo.getMotor() == "Hibrido"){
+                return 450000;
+            }
+            else if (vehiculo.getMotor() == "Electrico"){
+                return 0;
+            }
+
+        }
+        else if(reparacion.getTipoReparacion() == 7){
+            if(vehiculo.getMotor()=="Gasolina" || vehiculo.getMotor()=="Diesel" || vehiculo.getMotor()=="Hibrido" || vehiculo.getMotor()=="Electrico"){
+                return 100000;
+            }
+        }
+        else if(reparacion.getTipoReparacion() == 8){
+            if(vehiculo.getMotor() == "Gasolina" || vehiculo.getMotor() == "Diesel"){
+                return 180000;
+            }
+            else if(vehiculo.getMotor() == "Hibrido"){
+                return 210000;
+            }
+            else if(vehiculo.getMotor() == "Electrico"){
+                return 250000;
+            }
+
+        }
+        else if(reparacion.getTipoReparacion() == 9){
+            if(vehiculo.getMotor() == "Gasolina" || vehiculo.getMotor() == "Diesel"){
+                return 150000;
+            }
+            else if(vehiculo.getMotor() == "Hibrido" || vehiculo.getMotor() == "Electrico") {
+                return 180000;
+            }
+        }
+        else if(reparacion.getTipoReparacion() == 10){
+            if(vehiculo.getMotor() == "Gasolina"){
+                return 130000;
+            }
+            else if(vehiculo.getMotor() == "Diesel"){
+                return 140000;
+            }
+            else if(vehiculo.getMotor() == "Hibrido"){
+                return 220000;
+            }
+            else if (vehiculo.getMotor() == "Electrico"){
+                return 0;
+            }
+
+        }
+        else if(reparacion.getTipoReparacion() == 11){
+            if(vehiculo.getMotor()=="Gasolina" || vehiculo.getMotor()=="Diesel" || vehiculo.getMotor()=="Hibrido" || vehiculo.getMotor()=="Electrico"){
+                return 80000;
+            }
+        }
+
+        return 0;
+    }
+    public int recargoKilometraje (String recargo){
+        VehicleEntity vehiculo = vehicleRepository.findByPatenteQuery(recargo);
         if(vehiculo.getKilometraje()>=0 && vehiculo.getKilometraje()<5000){
             return 0;
         }
@@ -45,7 +170,7 @@ public class RepairService {
             return 4000;//Corregir!!!!
         }
 
-
+        return 0;
     }
 
 }
